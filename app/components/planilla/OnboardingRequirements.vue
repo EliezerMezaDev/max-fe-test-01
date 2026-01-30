7fdc71d026702acf41079d63a60b9e4c0d5fc81d
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2026-01-30 16:01:25 -0400

    fix: Omboarding requirements

--- app/components/planilla/OnboardingRequirements.vue
+++ app/components/planilla/OnboardingRequirements.vue
@@ -143,68 +143,30 @@ const props = defineProps<{
 
       <ul>
         <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Cédula de Identidad vigente para personas venezolanas y extranjeras
-          residentes en el país, o copia del pasaporte para personas extranjeras
-          no residentes.
+          <b class="text-maximiza-verde2 font-semibold"> 1.</b> Cédula de
+          Identidad vigente, para personas venezolanas y extranjeras residentes
+          en el país, o copia del pasaporte para personas extrajeras no
+          residente.
         </li>
         <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Registro de Información Fiscal (RIF) vigente.
+          <b class="text-maximiza-verde2 font-semibold">2.</b> Registro de
+          Información Fiscal (RIF), vigente.
         </li>
         <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Una (1) referencia bancaria, con una vigencia mínima de tres (3)
-          meses.
+          <b class="text-maximiza-verde2 font-semibold">3.</b> Una (1)
+          Referencia Bancaria, con una vigencia mínima de tres meses.
         </li>
         <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Constancia de trabajo actual del inversionista, o informe de
-          atestiguamiento de ingresos emitido por un contador público colegiado,
-          en caso de trabajadores independientes.
+          <b class="text-maximiza-verde2 font-semibold">4.</b> Constancia de
+          trabajo actual del inversor, o informe de atestiguamiento de ingresos
+          emitida por un contador público colegiado, en el caso de personas que
+          trabajen de manera independiente.
         </li>
+
         <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Planilla de Declaración Jurada de Origen y Destino de los Fondos, cada
-          vez que realice una operación.
-        </li>
-        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Planilla de Registro para la apertura de Subcuenta ante la Caja
-          Venezolana de Valores.
-        </li>
-        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Planilllas generadas por el formulario de registro del paso 1.
-        </li>
-        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Otros documentos: declaración de ISLR, recibo de servicio vigente y/o
-          referencias personales.
+          <b class="text-maximiza-verde2 font-semibold">5.</b> Planilla de
+          Declaración Jurada de Origen y Destino de los Fondos, cada vez que
+          realice una operación.
         </li>
       </ul>
     </article>
@@ -361,61 +323,81 @@ const props = defineProps<{
 
       <ul>
         <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Registro de Información Fiscal (RIF) vigente.
+          <b class="text-maximiza-verde2 font-semibold">1.</b> Registro de
+          Información Fiscal (RIF), vigente.
         </li>
         <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Cédula de Identidad y Registro de Información Fiscal (RIF) vigente del
-          representante legal.
+          <b class="text-maximiza-verde2 font-semibold">2.</b> Cédula de
+          Identidad y Registro de Información Fiscal (RIF) vigente del
+          representante legal de la persona jurídica.
         </li>
         <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Documento constitutivo y modificaciones estatutarias. En caso de
-          personas jurídicas no domiciliadas en el país, documentos y poderes
-          debidamente legalizados y traducidos al castellano por intérprete
-          público.
+          <b class="text-maximiza-verde2 font-semibold">3.</b> Documento
+          constitutivo y las modificaciones estatutarias de la persona jurídica
+          que muestre los socios beneficiarios finales de la sociedad. En caso
+          de persona jurídica no domiciliada en el país, los mismos documentos
+          debidamente protocolizados y traducidos al idioma castellano.
         </li>
         <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Acta de asamblea donde conste la elección de la junta directiva
-          actual, con período vigente.
+          <b class="text-maximiza-verde2 font-semibold">4.</b> Acta de asamblea
+          donde conste la elección de la junta directiva actual, sin que se
+          encuentre vencido su período electivo.
         </li>
         <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Una (1) referencia bancaria, con una vigencia mínima de tres (3)
-          meses.
+          <b class="text-maximiza-verde2 font-semibold">5.</b> Una (1)
+          Referencia Bancaria, con una vigencia mínima de tres meses.
         </li>
         <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Planilla de Declaración Jurada de Origen y Destino de los Fondos, cada
-          vez que realice una operación.
+          <b class="text-maximiza-verde2 font-semibold">6.</b> Planilla de
+          Declaración Jurada de Origen y Destino de los Fondos, cada vez que
+          realice una operación.
+        </li>
+      </ul>
+    </article>
+
+    <article>
+      <h5
+        class="text-maximiza-negro1 text-3xl font-bold flex items-center gap-2 mb-4"
+      >
+        Requisitos de las Asociaciones Cooperativas:
+      </h5>
+
+      <ul>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
+          <b class="text-maximiza-verde2 font-semibold">1.</b> Registro de
+          Información Fiscal (RIF), vigente.
         </li>
         <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-          <font-awesome-icon
-            class="text-maximiza-verde1 text-xl"
-            :icon="['fas', 'check']"
-          />
-          Planilla de Registro para la apertura de Subcuenta ante la Caja
-          Venezolana de Valores.
+          <b class="text-maximiza-verde2 font-semibold">2.</b> Cédula de
+          Identidad y Registro de Información Fiscal (RIF) vigente del
+          representante legal de la asociación cooperativa.
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
+          <b class="text-maximiza-verde2 font-semibold">3.</b> Constancia de
+          inscripción en la Superintendencia Nacional de Cooperativas
+          (SUNACOOP).
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
+          <b class="text-maximiza-verde2 font-semibold">4.</b> Certificado de
+          cumplimiento emanado de la SUNACOOP.
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
+          <b class="text-maximiza-verde2 font-semibold">5.</b> Acta constitutiva
+          y estatutos sociales debidamente protocolizados ante el Registro
+          Público y, cuando aplique, sus reformas. En el caso de las
+          Asociaciones Cooperativas no domiciliadas en el país, los mismos
+          documentos debidamente protocolizados y traducidos al idioma
+          castellano.
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
+          <b class="text-maximiza-verde2 font-semibold">6.</b> Acta de asamblea
+          donde conste la elección de la junta directiva actual, sin que se
+          encuentre vencido su período electivo.
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
+          <b class="text-maximiza-verde2 font-semibold">7.</b> Listado de
+          asociados activos, emitido por la junta directiva de la asociación
+          cooperativa.
         </li>
       </ul>
     </article>
@@ -427,37 +409,45 @@ const props = defineProps<{
         Registro para fideicomiso
       </h5>
 
-      <p class="mt-2 mb-6 text-lg text-maximiza-gris1">
-        Para Persona Jurídica, también
-        <b class="font-extrabold">
-          se contemplan los siguientes tipos de registro, con sus recaudos
-          específicos:
-        </b>
-      </p>
-
-      <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-        <font-awesome-icon
-          class="text-maximiza-verde1 text-xl"
-          :icon="['fas', 'download']"
-        />
-        Requisitos para los fideicomisos
-      </li>
-
-      <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-        <font-awesome-icon
-          class="text-maximiza-verde1 text-xl"
-          :icon="['fas', 'download']"
-        />
-        Requisitos para las asociaciones cooperativas
-      </li>
-
-      <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
-        <font-awesome-icon
-          class="text-maximiza-verde1 text-xl"
-          :icon="['fas', 'download']"
-        />
-        Requisitos para las OSLFL
-      </li>
+      <ul>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
+          <b class="text-maximiza-verde2 font-semibold">1.</b> Cédula de
+          Identidad y Registro de Información Fiscal (RIF) vigente del
+          fideicomitente, fiduciario y terceros intervinientes.
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
+          <b class="text-maximiza-verde2 font-semibold">2.</b> Contrato de
+          fideicomiso.
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start">
+          <b class="text-maximiza-verde2 font-semibold">3.</b> Carta explicativa
+          de inversión o memoria descriptiva, indicando los motivos de la
+          inversión de ser el caso:
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start ml-6">
+          <b class="text-maximiza-verde2 font-semibold">a.</b> Partes
+          necesarias:
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start ml-12">
+          <b class="">i.</b> Quien trasmite los bienes (Fideicomitente), y
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start ml-12">
+          <b class="">ii.</b> Quien recibe y adquiere los bienes en propiedad
+          fiduciaria (Fiduciario).
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start ml-6">
+          <b class="text-maximiza-verde2 font-semibold">b.</b> Terceros
+          interesados:
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start ml-12">
+          <b class="">i.</b> Quien recibe los bienes del fideicomiso
+          (Beneficiario), y
+        </li>
+        <li class="text-lg text-maximiza-gris1 flex gap-2 items-start ml-12">
+          <b class="">ii.</b> El destinatario final de los bienes del
+          fideicomiso (Fideicomisario).
+        </li>
+      </ul>
     </article>
 
     <article>
